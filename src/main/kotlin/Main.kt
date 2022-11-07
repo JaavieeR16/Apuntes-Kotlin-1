6dfce077629b8f4de67eca69aa7efa90c13d8251
Javier <javiermonroym16@gmail.com>
2022-11-07 16:55:15 +0100

    Apuntes v.1.6 7-11-22

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -14,4 +14,6 @@ fun main(args: Array<String>) {
     //arrays()
 
     //maps()
+
+    //loops()
 }
--- src/main/kotlin/ejercicios/ejercicios.kt
+++ src/main/kotlin/ejercicios/ejercicios.kt
@@ -271,3 +271,82 @@ fun maps(){
     println(myMap)
 
 }
+
+//Bucles o Loops
+
+fun loops(){
+
+    //Bucles
+
+    val myArray = listOf("Hola", "Bienvenidos al mundo", "Programacion")
+    val myMap = mutableMapOf("Javier" to 1, "Pedro" to 2, "Sara" to 3)
+
+    //For
+
+    for (myString in myArray){
+        println(myString)
+    }
+
+    for (myElement in myMap){
+        println("${myElement.key}-${myElement.value}")
+    }
+
+    for (x in 0..10){
+        println(x)
+    }
+
+    for (x in 0 until 10){
+        println(x)
+    }
+
+    for (x in 0..10 step 2){
+        println(x)
+    }
+
+    for (x in 10 downTo 0 step 3){
+        println(x)
+    }
+
+    val myNumericArray = (0..20)
+
+    for (myNum in myNumericArray){
+        println(myNum)
+    }
+
+    //While
+
+    var x = 0
+
+    while (x < 10){
+        println(x)
+        x += 2
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
